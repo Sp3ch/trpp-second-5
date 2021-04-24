c8136761fcafa6d5bdf92ec6b0bf4e968030f688
Pereverzev <pereverzev@nothing.com>
2021-04-24 10:36:55 +0300

    1

--- src/main/java/ru/mirea/trppsecond5/controllers/MedicineController.java
+++ src/main/java/ru/mirea/trppsecond5/controllers/MedicineController.java
@@ -1,10 +1,10 @@
-package ru.mirea.trpp_second_5.controllers;
+package ru.mirea.trppsecond5.controllers;
 
 import com.opencsv.bean.CsvToBeanBuilder;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
-import ru.mirea.trpp_second_5.entity.Medicine;
+import ru.mirea.trppsecond5.entity.Medicine;
 
 import java.io.InputStreamReader;
 import java.util.List;
